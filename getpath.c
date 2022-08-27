f5a2a0e995f6124891bb3c173e261c8c93a4f97e
Kabir <Katoyebir@gmail.com>
2022-08-27 15:14:58 -0700

    freed check and fixed buf

--- getpath.c
+++ getpath.c
@@ -3,16 +3,18 @@ char *getpath(char *cmd)
 {
 	extern char **environ;
 	int i, k = _strlen(cmd), l, m;
-	char *path = _getenv("PATH"), *buf, *check, **PATH;
+	char *path = _getenv("PATH"), *buf, *check = NULL, **PATH;
 
 	buf = malloc(sizeof(char) * _strlen(path) + 1);
-	buf[i] = '\0';
+	buf[_strlen(path)] = '\0';
 	_strcpy(path, buf);
 	for(i = 0; buf[i] != '/'; ++i)
 		buf[i] = ':';
 	PATH = extract_args(buf, ':', count_args(buf, ':'));
 	for (i = 0; PATH[i] != NULL; ++i)
 	{
+		if (check)
+			free(check);
 		check = malloc(sizeof(char) * (_strlen(PATH[i]) + k + 2));
 		for (l = 0; PATH[i][l]; ++l)
 			check[l] = PATH[i][l];
