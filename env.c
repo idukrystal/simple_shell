a8cb767a4f1a685c6f5549c1ebe554e0e1fdf2dd
Kabir <Katoyebir@gmail.com>
2022-08-29 13:02:11 -0700

    completed setenv

--- env.c
+++ env.c
@@ -1,23 +1,54 @@
 #include "main.h"
-void setenv(char *env, int *eflag)
+extern char **environ;
+int  _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+void printenv(void)
+{
+	int i;
+
+	for (i = 0; environ[i]; ++i)
+	{
+		write(1, environ[i], _strlen(environ[i]));
+		_putchar('\n');
+	}
+}
+int _strlen(const char *p)
+{
+	int i;
+
+	for (i = 0; p[i]; ++i)
+		;
+	return (i);
+}
+void _setenv(char *env, int *eflag)
 {
 	int i, j;
+	int c = 0;
 	char **newenv;
 	for (i = 0; environ[i]; ++i)
 		;
 	newenv = malloc(sizeof(*environ) * i + 2);
 	newenv[i] = malloc(sizeof(char) * _strlen(env) + 1);
-	for (j = 0; env[j]; ++i)
+	for (j = 0; env[j]; ++j)
 		newenv[i][j] = env[j];
 	newenv[i][j] = '\0';
 	newenv[i + 1] = NULL;
 	for (i = 0; environ[i]; ++i)
 	{
+		printf("%d\n", c++);
 		newenv[i] = malloc(sizeof(char) * _strlen(environ[i]) + 1);
 		for (j = 0; environ[i][j]; ++j)
 			newenv[i][j] = environ[i][j];
 		newenv[i][j] = '\0';
 	}
-	eflag = 1;
+	*eflag = 1;
 	environ = newenv;
+}
+void main(void)
+{
+	int m;
+	_setenv("hello", &m);
+	printenv();
 }
--- main.h
+++ main.h
@@ -32,5 +32,6 @@ unsigned int  print_u_number(unsigned int n);
 int _putchar(char );
 unsigned int to_base(unsigned int,char);
 char *getvar(char *var);
+void _setenv(char *env, int *eflag);
 
 #endif /* main_h */
