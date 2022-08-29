b69360f81bcd3a45e90ecfc010d1b48ff0676f75
Kabir <Katoyebir@gmail.com>
2022-08-29 13:54:40 -0700

    set and unset env

--- env.c
+++ env.c
@@ -22,33 +22,54 @@ int _strlen(const char *p)
 		;
 	return (i);
 }
-void _setenv(char *env, int *eflag)
+void _setenv(char *env, char *vne, int *eflag)
 {
-	int i, j;
-	int c = 0;
+	int i, j, k;
 	char **newenv;
+
+	if (getenv(env) != NULL)
+		return;
 	for (i = 0; environ[i]; ++i)
 		;
 	newenv = malloc(sizeof(*environ) * i + 2);
-	newenv[i] = malloc(sizeof(char) * _strlen(env) + 1);
+	newenv[i] = malloc(sizeof(char) * (_strlen(env) + _strlen(vne) + 1));
 	for (j = 0; env[j]; ++j)
 		newenv[i][j] = env[j];
+	newenv[i][j++] = '=';
+	for (k = 0; vne[k]; ++k, ++j)
+		newenv[i][j] = vne[k];
 	newenv[i][j] = '\0';
 	newenv[i + 1] = NULL;
 	for (i = 0; environ[i]; ++i)
 	{
-		printf("%d\n", c++);
 		newenv[i] = malloc(sizeof(char) * _strlen(environ[i]) + 1);
 		for (j = 0; environ[i][j]; ++j)
 			newenv[i][j] = environ[i][j];
 		newenv[i][j] = '\0';
 	}
+	/*
+	if (*eflag == 1)
+		free_args(environ);
+		*/
+	/* remember to add error messages*/
 	*eflag = 1;
 	environ = newenv;
 }
+void _unsetenv(char *env, int *eflag)
+{
+	int i, j, index;
+
+	index = _getindex(env);
+	if (index == -1)
+		return;
+	if (*eflag == 1)
+		free(environ[index]);
+	for (; environ[index]; ++index)
+		environ[index] = environ[index + 1];
+}
 void main(void)
 {
-	int m;
-	_setenv("hello", &m);
+	int m = 0;
+	_unsetenv("TERM", &m);
 	printenv();
 }
--- getenv.c
+++ getenv.c
@@ -19,5 +19,27 @@ char *_getenv(const char *name)
 			}
 		}
 	}
-	return ("null");
+	return (NULL);
+}
+int _getindex(const char *name)
+{
+	extern char **environ;
+	int i = -1, j;
+
+	while (environ[++i] != NULL)
+	{
+		j = 0;
+		if (environ[i][j] == name[j])
+		{
+			while(environ[i][j] == name[j])
+			{
+				j++;
+			}
+			if (environ[i][j] == '=' && name[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
 }
