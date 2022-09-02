498ba3f86c4fe118d5c6b694857fc52e51cdc569
Kabir <Katoyebir@gmail.com>
2022-09-02 13:02:52 -0700

    betty doc

--- func2.c
+++ func2.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+/**
+ * count - counts
+ * @args: string
+ * Return: number of count
+ */
 int count(char **args)
 {
 	int i = 0;
 
-	while(args[i] != NULL)
+	while (args[i] != NULL)
 		i++;
 	return (i);
 }
